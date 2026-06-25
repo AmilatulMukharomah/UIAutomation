98e1de7aa075e4c499386aade3e4ede6d85d1174
AmilatulMukharomah <ayuni238@gmail.com>
2026-06-25 10:57:32 +0700

    assert sorting A to Z

--- tests/sesi9/test_sesi9.js
+++ tests/sesi9/test_sesi9.js
@@ -33,4 +33,35 @@ describe('Test Login SauceDemo', function() {
         await buttonLogin.click();
     });
 
+    it('3. Display main page and verify cart button', async function() {
+        // wait element appear
+        let buttonCart = await driver.wait(
+            until.elementLocated(By.xpath('//*[@data-test="shopping-cart-link"]')), 
+            10000
+        );
+        await driver.wait(until.elementIsVisible(buttonCart), 5000, 'Button Cart is not visible');
+
+        // Assert that the button cart is displayed
+        let isButtonCartDisplayed = await buttonCart.isDisplayed();
+        assert.strictEqual(isButtonCartDisplayed, true, 'Button Cart is not displayed');
+    });
+
+    it('4. Display and test sorting dropdown', async function() {
+        let dropdown = await driver.findElement(By.xpath('//*[@data-test="product-sort-container"]'));
+        
+        // click dropdown and select Z to A
+        await dropdown.click();
+        await driver.findElement(By.xpath('//option[text()="Name (Z to A)"]')).click();
+        await driver.sleep(1000); // Jeda agar transisi terlihat
+
+        // select A to Z
+        await driver.findElement(By.xpath('//option[text()="Name (A to Z)"]')).click();
+
+        // Assert dropdown A to Z
+        let selectedOption = await driver.findElement(By.xpath('//option[text()="Name (A to Z)"]'));
+        let selectedText = await selectedOption.getText();
+        assert.strictEqual(selectedText, 'Name (A to Z)', 'Dropdown selection is not correct');
+    });
+
+
 });
